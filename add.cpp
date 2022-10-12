3096d0e49a0181f832137fd77a149095fe9dba76
Ayaansk49 <113667254+Ayaansk49@users.noreply.github.com>
2022-10-12 21:49:47 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+using namespace std;
+int main()
+
+{
+
+    string sent ="Hi I am hardik Lad";
+    cout<<sent.substr(1,9);
+
+}
---
+++ average.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       int a,b,c,avg;
+
+       cout<<"Enter the three numbers:"<<endl;
+       cin>>a>>b>>c;
+
+       avg = (a+b+c)/3;
+
+       cout<<"The average of three numbers are "<<avg<<endl;
+
+       return 0;
+
+}
